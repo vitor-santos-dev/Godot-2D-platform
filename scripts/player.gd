c80b193a5e0b51c8988f0f240c4f1fbc1464d924
Vitor Augusto dos Santos <vitor.augusto1@aluno.ifsp.edu.br>
2025-12-25 06:10:33 -0300

    Basic sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
+@onready var player_sprites: AnimatedSprite2D = $"player-sprites"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 30.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -17,9 +18,19 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_on_floor():
+		if direction == -1:
+			velocity.x = direction * SPEED
+			player_sprites.flip_h = true
+			player_sprites.play("walk")
+		elif direction == 1:
+			velocity.x = direction * SPEED
+			player_sprites.flip_h = false
+			player_sprites.play("walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			player_sprites.play("idle")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		player_sprites.play("jump")
 
 	move_and_slide()
